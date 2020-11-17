dea9329bd0fcd03988bb84990fb673ecbf729279
Danilo Otavio Lima Salve <danilo.salve@sp01.local>
2020-11-17 20:24:05 -0300

    imelhoria no catagoryList component

--- src/app/pages/categories/category-list/category-list.component.ts
+++ src/app/pages/categories/category-list/category-list.component.ts
@@ -1,4 +1,8 @@
+import { element } from 'protractor';
 import { Component, OnInit } from '@angular/core';
+
+import { Category } from '../shared/category.model';
+import { CategoryService } from './../shared/category.service';
 
 @Component({
   selector: 'app-category-list',
@@ -7,13 +11,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoryListComponent implements OnInit {
 
-  constructor() { }
+  public categories: Category[] = [];
+
+  constructor(
+    private categoryService: CategoryService
+  ) { }
 
   ngOnInit(): void {
+    this.categoryService.getAll().subscribe(
+      categories => this.categories = categories,
+      error => alert('Error ao Cattegar a lista')
+    );
   }
 
-  excluir(): void {
-    alert('excluir');
+  deleteCategory(category): void {
+    const mustDelete = confirm('Deleja realmente excluir este item?');
+
+    if (mustDelete) {
+      this.categoryService.delete(category.id).subscribe(
+        () => this.categories = this.categories.filter(element => element != category),
+        () => alert('Erro ao tentar excluir!!!')
+      );
+    }
   }
 
 }
