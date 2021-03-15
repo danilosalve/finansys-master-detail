70fad633bf3980ba94f6ae3884805ced4038fd18
Danilo Otavio Lima Salve <danilo.salve@totvs.com.br>
2021-03-15 16:31:52 -0300

    improving EntryList

--- src/app/pages/entries/entry-list/entry-list.component.ts
+++ src/app/pages/entries/entry-list/entry-list.component.ts
@@ -9,7 +9,7 @@ import { EntryService } from './../shared/entry.service';
   templateUrl: './entry-list.component.html',
   styleUrls: ['./entry-list.component.css']
 })
-export class EntryListComponent implements extends BaseResourceListComponent<Entry> {
+export class EntryListComponent extends BaseResourceListComponent<Entry> {
 
   constructor(
     private entryService: EntryService
