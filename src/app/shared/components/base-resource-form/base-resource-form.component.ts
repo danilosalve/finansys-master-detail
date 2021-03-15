1594335d8eca620ebaf4d882730ce2590639d680
Danilo Otavio Lima Salve <danilo.salve@totvs.com.br>
2021-03-15 12:05:59 -0300

    Adding BaseResourceFormComponent

---
+++ src/app/shared/components/base-resource-form/base-resource-form.component.ts
@@ -0,0 +1,136 @@
+
+import { OnInit, AfterContentChecked, Injector } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
+
+import toastr from 'toastr';
+
+import { BaseResourceModel } from '../../models/base-resource.model';
+import { BaseResourceService } from '../../services/base-resource.service';
+
+export abstract class BaseResourceFormComponent<T extends BaseResourceModel> implements OnInit, AfterContentChecked {
+  currentAction: string;
+  resourceForm: FormGroup;
+  pageTitle: string;
+  serverErrorMessages: string[] = null;
+  submittingForm = false;
+
+  protected route: ActivatedRoute;
+  protected router: Router;
+  protected formBuilder: FormBuilder;
+
+  constructor(
+    protected injector: Injector,
+    public resource: T,
+    protected resourceService: BaseResourceService<T>,
+    protected jsonDataToResourceFn: (jsonData) => T
+  ) {
+    this.route = this.injector.get(ActivatedRoute);
+    this.router = this.injector.get(Router);
+    this.formBuilder = this.injector.get(FormBuilder);
+  }
+
+  ngOnInit(): void {
+    this.setCurrentAction();
+    this.buildResourceForm();
+    this.loadResource();
+  }
+
+  ngAfterContentChecked(): void {
+    // Carrega depois que todo componente foi iniciado
+    this.setPageTitle();
+  }
+
+  submitForm(): void {
+    this.submittingForm = true;
+    if (this.currentAction === 'new') {
+      this.createResource();
+    } else {
+      this.updateResource();
+    }
+  }
+
+  protected setCurrentAction(): void {
+    if (this.route.snapshot.url[0].path === 'new') {
+      this.currentAction = 'new';
+    } else {
+      this.currentAction = 'edit';
+    }
+  }
+
+  protected loadResource(): void {
+    if (this.currentAction === 'edit') {
+      this.route.paramMap.pipe(
+        switchMap(params => this.resourceService.getById(+params.get('id')))
+      )
+      // tslint:disable-next-line: deprecation
+      .subscribe(
+        resource => {
+          this.resource = resource;
+          this.resourceForm.patchValue(resource);
+      },
+      () => alert('Ocorreu um erro no servidor, tenta mais tarde.')
+      );
+    }
+  }
+
+  protected setPageTitle(): void {
+    if (this.currentAction === 'new') {
+      this.pageTitle = this.creationPageTitle();
+    } else {
+      this.pageTitle = this.editionPageTitle();
+    }
+  }
+
+  protected creationPageTitle(): string {
+    return 'Novo';
+  }
+
+  protected editionPageTitle(): string {
+    return 'Edição';
+  }
+
+  protected createResource(): void {
+    const resource: T = this.jsonDataToResourceFn(this.resourceForm.value);
+    this.resourceService.create(resource)
+      // tslint:disable-next-line: deprecation
+      .subscribe(
+        res => this.actionsForSucess(res),
+        error => this.actionsForError(error)
+      );
+  }
+
+  protected updateResource(): void {
+    const resource: T = this.jsonDataToResourceFn(this.resourceForm.value);
+    this.resourceService.update(resource)
+    // tslint:disable-next-line: deprecation
+    .subscribe(
+      res => this.actionsForSucess(res),
+      error => this.actionsForError(error)
+    );
+  }
+
+  protected actionsForSucess(resource: T): void {
+    toastr.success('Solicitacao processada com sucesso');
+
+    const baseComponentPath: string = this.route.snapshot.parent.url[0].path;
+
+    this.router.navigateByUrl(baseComponentPath, {skipLocationChange: true}).then(
+      () => this.router.navigate([baseComponentPath, resource.id, 'edit'])
+    );
+  }
+
+  protected actionsForError(error: any): void {
+    toastr.error('Ocorreu um erro ao processar a sua solicitação!');
+    this.submittingForm = false;
+    if (error.status === 422) {
+      this.serverErrorMessages = JSON.parse(error._body).errors;
+    } else {
+      this.serverErrorMessages = ['Falha na comunicação com o servidor. Por favor tente mais tarde.'];
+    }
+  }
+
+  protected abstract buildResourceForm(): void;
+
+}
