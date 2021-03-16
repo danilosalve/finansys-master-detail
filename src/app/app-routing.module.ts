985d5fc6de709491d2bb15588cbbb1f2fcee4496
Danilo Otavio Lima Salve <danilo.salve@totvs.com.br>
2021-03-16 12:10:56 -0300

    adding reports

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,16 +6,23 @@ const routes: Routes = [
     path: 'entries',
     loadChildren: () =>
       import('./pages/entries/entries.module').then(
-        (m) => m.EntriesModule
+        m => m.EntriesModule
       ),
   },
   {
     path: 'categories',
     loadChildren: () =>
       import('./pages/categories/categories.module').then(
-        (m) => m.CategoriesModule
+        m => m.CategoriesModule
       ),
   },
+  {
+    path: 'reports',
+    loadChildren: () =>
+      import('./pages/reports/reports.module').then(
+        m => m.ReportsModule
+      )
+  }
 ];
 
 @NgModule({
